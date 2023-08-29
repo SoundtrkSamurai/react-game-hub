61044f3ca8f203b2dddd4ad016395f2436e210fe
SoundtrkSamurai <furtimat3@yahoo.com>
2023-08-29 15:07:13 -0700

    filter: build platform selector.

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -12,7 +12,7 @@ interface Props {
 const GameGrid = ({ selectedGenre }: Props) => {
   const { data, error, isLoading } = useGames(selectedGenre);
   const skeletons = [
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 18, 20,
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
   ];
 
   return (
--- src/types.ts
+++ src/types.ts
@@ -32,3 +32,9 @@ export interface FetchResponse<T> {
   count: number;
   results: T[];
 }
+
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}

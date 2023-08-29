c11fef65b096401535636452be26778264a9e9d1
SoundtrkSamurai <furtimat3@yahoo.com>
2023-08-29 15:44:11 -0700

    filter: filter games by platform

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import { Grid, GridItem, Show } from "@chakra-ui/react";
-import "./App.scss";
-import NavBar from "./components/NavBar";
-import PlatformSelector from "./components/PlatformSelector";
-import GameGrid from "./components/GameGrid";
-import GenreList from "./components/GenreList";
-import { useState } from "react";
-import { Genre } from "./types";
+import { Grid, GridItem, Show } from '@chakra-ui/react';
+import './App.scss';
+import NavBar from './components/NavBar';
+import PlatformSelector from './components/PlatformSelector';
+import GameGrid from './components/GameGrid';
+import GenreList from './components/GenreList';
+import { useState } from 'react';
+import { Genre, Platform } from './types';
 
 function App() {
   const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
+  const [selectedPlatform, setSelectedPlatform] = useState<Platform | null>(
+    null
+  );
 
   return (
     <Grid
@@ -17,24 +20,30 @@ function App() {
         lg: `"nav nav" "aside main"`, // 1024px
       }}
       templateColumns={{
-        base: "1fr",
-        lg: "200px 1fr",
+        base: '1fr',
+        lg: '200px 1fr',
       }}
     >
-      <GridItem area="nav">
+      <GridItem area='nav'>
         <NavBar />
       </GridItem>
-      <Show above="lg">
-        <GridItem area="aside" paddingX={5}>
+      <Show above='lg'>
+        <GridItem area='aside' paddingX={5}>
           <GenreList
             selectedGenre={selectedGenre}
             onSelectGenre={(genre) => setSelectedGenre(genre)}
           />
         </GridItem>
       </Show>
-      <GridItem area="main">
-        <PlatformSelector />
-        <GameGrid selectedGenre={selectedGenre} />
+      <GridItem area='main'>
+        <PlatformSelector
+          onSelectPlatform={(platform) => setSelectedPlatform(platform)}
+          selectedPlatform={selectedPlatform}
+        />
+        <GameGrid
+          selectedPlatform={selectedPlatform}
+          selectedGenre={selectedGenre}
+        />
       </GridItem>
     </Grid>
   );

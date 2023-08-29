5bfb102271623e510b2c384d8aa1e3594230ab12
SoundtrkSamurai <furtimat3@yahoo.com>
2023-08-28 20:17:08 -0700

    ui: displaying platform icons

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,14 @@
-export interface Game {
+ export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+ }
+
+ export interface Game {
   id: number;
   name: string;
   background_image: string;
+  parent_platforms: { platform: Platform }[]
 }
 
 export interface FetchGamesResponse {

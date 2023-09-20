ec5a366083e47675719340be820ee00c4bd24d53
SoundtrkSamurai <furtimat3@yahoo.com>
2023-09-19 17:15:14 -0700

    react-query: remove obsolete types

--- src/types.ts
+++ src/types.ts
@@ -12,20 +12,10 @@
   metacritic: number;
 }
 
-export type FetchGamesResponse = {
-  count: number;
-  results: Game[];
-}
-
 export type Genre = {
   id: number;
   name: string;
   image_background: string;
-}
-
-export type FetchGenresResponse = {
-  count: number;
-  results: Genre[];
 }
 
 export type FetchResponse<T> = {

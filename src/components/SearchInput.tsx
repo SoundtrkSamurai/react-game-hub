46572b00888c5e6bb31b15e15ebf61f30533b400
SoundtrkSamurai <furtimat3@yahoo.com>
2023-09-25 21:14:51 -0700

    react-router: Fix the nav bar.

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,16 +1,21 @@
 import { Input, InputGroup, InputLeftElement } from '@chakra-ui/react';
 import { useRef } from 'react';
 import { BsSearch } from 'react-icons/bs';
+import { useNavigate } from 'react-router-dom';
 import useGameQueryStore from '../state-management/useGameQueryStore';
 
 const SearchInput = () => {
   const setSearchText = useGameQueryStore((s) => s.setSearchText);
   const refSearch = useRef<HTMLInputElement>(null);
+  const navigate = useNavigate();
   return (
     <form
       onSubmit={(event) => {
         event.preventDefault();
-        if (refSearch.current) setSearchText(refSearch.current.value);
+        if (refSearch.current) {
+          setSearchText(refSearch.current.value);
+          navigate('/');
+        }
       }}
     >
       <InputGroup>
